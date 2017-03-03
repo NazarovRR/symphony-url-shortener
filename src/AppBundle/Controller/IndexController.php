ff1f53031358858b5c585f2f56c3ab169dc0fc86
Mike <nazarovmichaelrr@gmail.com>
2017-03-03 12:13:08 +0300

    moved json api method in separate controller

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -9,14 +9,11 @@
 namespace AppBundle\Controller;
 
 
-use AppBundle\Entity\Url;
 use AppBundle\Form\PostFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller
 {
@@ -56,45 +53,5 @@ class IndexController extends Controller
         } else {
             return $this->redirect("/");
         }
-    }
-
-    /**
-     * @Route("/api/v1/create")
-     * @Method("POST")
-     */
-    public function createAction(Request $request)
-    {   $params = array();
-        $body = $request->getContent();
-        if (!empty($body))
-        {
-            $params = json_decode($body, true);
-        }
-        $model = new Url();
-        $model->setFullUrl(@$params['full_url']);
-        if(@$params['encoded'])
-        {
-            $model->setEncoded($params['encoded']);
-        }
-        $validator = $this->get("validator");
-        $violations = $validator->validate($model);
-        if (count($violations) > 0) {
-            $errors = array();
-            foreach ($violations as &$error) {
-                array_push($errors,$error->getMessage());
-            }
-            unset($error);
-            $data = array(
-                "errors"=>$errors,
-                "message"=>"validation error"
-            );
-            return new JsonResponse($data,400);
-        }
-        $updater = $this->container->get('encodeUpdater');
-        $model = $updater->getEncodedModel($model);
-        $data = array(
-            "encoded"=>$model->getEncoded(),
-            "message"=>"Success"
-        );
-        return new JsonResponse($data);
     }
 }
